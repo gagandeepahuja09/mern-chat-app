6af02e73d81a87d7590f41ef75afb9e055216b8e
Gagandeep Ahuja <gagandeep.ahuja@oyorooms.com>
2020-05-19 17:51:52 +0530

    setting up router and testing by using it as middleware

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+
+const router = require('./router');
+
+const PORT = process.env.PORT || 5000
+
+const app = express();
+// Initialize a server for app
+const server = http.createServer(app);
+// Create a socket.io server
+const io = socketio(server);
+
+// Use this router as middleware
+app.use(router);
+
+server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
