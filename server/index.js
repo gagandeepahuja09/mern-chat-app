8596d12b1223ec4331dd75adc3c44231d1a1fa71
gagandeepahuja09 <gagandeepahuja09@gmail.com>
2020-05-20 21:57:25 +0530

    fixed minor bug

--- server/index.js
+++ server/index.js
@@ -1,4 +1,3 @@
-
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
@@ -19,7 +18,7 @@ io.on('connect', (socket) => {
   socket.on('join', ({ name, room }, callback) => {
     const { error, user } = addUser({ id: socket.id, name, room });
 
-    if(error) return callback(error);
+    // if(error) return callback(error);
 
     socket.join(user.room);
 
@@ -27,19 +26,20 @@ io.on('connect', (socket) => {
     socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined!` });
 
     io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
+
+    // callback();
   });
 
   socket.on('sendMessage', (message, callback) => {
     const user = getUser(socket.id);
-
-    io.to(user.room).emit('message', { user: user.name, text: message });
-
+    if(user) {
+      io.to(user.room).emit('message', { user: user.name, text: message });
+    }
     callback();
   });
 
   socket.on('disconnect', () => {
     const user = removeUser(socket.id);
-
 
     if(user) {
       io.to(user.room).emit('message', { user: 'Admin', text: `${user.name} has left.` });
