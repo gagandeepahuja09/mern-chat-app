1bfccad064d76473d7dc56a96f9fc6e6e7f3eb73
gagandeepahuja09 <gagandeepahuja09@gmail.com>
2020-05-20 18:38:51 +0530

    sending message from frontend

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -2,6 +2,12 @@ import React, { useState, useEffect } from 'react';
 import queryString from "query-string";
 import io from "socket.io-client";
 
+
+import InfoBar from '../InfoBar/InfoBar';
+import Input from '../Input/Input';
+
+import './Chat.css'
+
 let socket;
 
 const Chat = ({ location }) => {
@@ -54,10 +60,9 @@ const Chat = ({ location }) => {
     return (
         <div className = "outerContainer">
             <div className = "container">
-                <input value = {message} 
-                onChange = { (event) => setMessage(event.target.value) }
-                onKeyPress = { (event) => event.key === 'Enter' ? sendMessage(event) : null}
-                />
+                <InfoBar room = { room } />
+
+                <Input message = { message } setMessage = { setMessage } sendMessage = { sendMessage } />
             </div>
         </div>
     );
